6c51d54460b8fc92cdc651d019137eeece4eee86
GustavoFavorito <gustavofavorito.d@gmail.com>
2024-04-09 18:18:16 -0300

    adição da tela de atualização

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header'
 import ReceitasSection from './components/receitas/ReceitasSection'
-import Cadastro from './components/Cadastro'
+import Cadastro from './components/cadastro/Cadastro'
 
 function App() {
   return (
