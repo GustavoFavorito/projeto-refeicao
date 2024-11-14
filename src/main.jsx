8776166aa2740cbeceac0df342ffd3557b2e2191
GustavoFavorito <gustavofavorito.d@gmail.com>
2024-11-14 18:37:08 -0300

    ajustes do deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
